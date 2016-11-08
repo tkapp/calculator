43c4aff5a09b920ee793f766a13a221ea75868a3
ms <ms@ms-PC>
2016-11-08 22:01:46 +0900

    first commit

---
+++ src/main/scala/jp/tkapp/caluculator/Main.scala
@@ -0,0 +1,17 @@
+
+package jp.tkapp.caluculator
+
+/**
+ * 実行クラスです。
+ */
+object Main {
+  
+  /**
+   * 実行します。
+   * 
+   * @param args 実行時引数
+   */
+  def main(args: Array[String]): Unit = {
+    println("h w")
+  }
+}
